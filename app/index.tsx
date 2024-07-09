72317367260272003a016308bc6fae9874d43bff
UmeshaWickramarachchi <umeshapubudi57@gmail.com>
2024-07-09 01:36:38 -0700

    create home page

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={{ backgroundColor: '#FCBB4B', flex: 1 }}>
+      {/* Your other components here */}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  
+  
+});
